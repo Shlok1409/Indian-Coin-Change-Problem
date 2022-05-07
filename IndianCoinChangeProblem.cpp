eb29b9f74e39320b8912da18a11e322d78ef4226
Shlok1409 <81183464+Shlok1409@users.noreply.github.com>
2022-05-08 03:44:50 +0530

    Add files via upload

---
+++ IndianCoinChangeProblem.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long minCoins(long long arr[],long long n,long long X)
+{
+    sort(arr,arr + n);
+    long long coins = 0;
+    long long i = n - 1;
+
+    while(i >= 0)
+    {
+        coins = coins + X/arr[i];
+        X = X % arr[i];
+        i--;
+    }
+
+    return coins;
+}
+
+int main()
+{
+    long long n;
+    cin>>n;
+    long long arr[n];
+    for(long long i = 0; i < n ; i++)
+        cin>>arr[i];
+    long long X;
+    cin>>X;
+    cout<<minCoins(arr,n,X)<<endl;
+    return 0;
+}
